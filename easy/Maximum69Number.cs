57aaf1ee5ad02b238a9d7a528d88f34574c8e89a
Angelmmc <medinacardenasam@gmail.com>
2025-08-16 15:29:57 -0600

    Add project files.

---
+++ easy/Maximum69Number.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_cs.easy
+{
+    internal class Maximum69Number
+    {
+        public static int GetMaximum69Number(int num)
+        {
+            string text = num.ToString();
+            int maxNumber = num;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                string remove = text.Remove(i, 1);
+
+                string add = (c == '9') ? remove.Insert(i, "6") : remove.Insert(i, "9");
+
+                if (int.Parse(add) > maxNumber)
+                {
+                    maxNumber = int.Parse(add);
+                }
+            }
+
+            return maxNumber;
+        }
+
+        public static void main()
+        {
+            int num = 9669;
+
+            Console.WriteLine(GetMaximum69Number(num));
+        }
+    }
+}
